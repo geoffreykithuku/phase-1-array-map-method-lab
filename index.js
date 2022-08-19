e096a329c6e05ee4343449c18a617c0c6f96b4b6
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-08-19 16:35:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+	'what does the this keyword mean?',
+	'What is the Constructor OO pattern?',
+	'implementing Blockchain Web API',
+	'The Test Driven Development Workflow',
+	'What is NaN and how Can we Check for it',
+	'What is the difference between stopPropagation and preventDefault?',
+	'Immutable State and Pure Functions',
+	'what is the difference between == and ===?',
+	'what is the difference between event capturing and bubbling?',
+	'what is JSONP?'
 ];
 
-// function convertFirstLetterToUpperCase(str) {
-//    return str
-//     .toLowerCase()
-//     .split(' ')
-//     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-//     .join(' ');
-// }
-const titleCased = (turorials) => {
-  for (let i = 0; i < tutorials.length, i++){
-    let str = tutorials[i];
-    
-    const converted = function(str) {
-   return str
-    .toLowerCase()
-    .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ');
-}
-    
-  }
+
+const titleCased = () => {
+	return tutorials.map(tutorial => {
+		const splitSentenceIntoWords = tutorial.split(' '); 
+		const capitalizedWords = splitSentenceIntoWords.map(splitSentenceIntoWords => splitSentenceIntoWords.charAt(0).toUpperCase() + splitSentenceIntoWords.slice(1)); 
+		const titleCasedTutorial = capitalizedWords.join(' '); 
+		return titleCasedTutorial; 
+	})
 }
